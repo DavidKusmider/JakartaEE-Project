19bc12edf2386ca97979109a18134addc21c329d
Romain Barré <rombar999@gmail.com>
2023-10-31 17:57:16 +0100

    admin : add check for client

---
+++ src/main/java/controller/servlets/AdminServlet.java
@@ -0,0 +1,28 @@
+package controller.servlets;
+
+import java.io.*;
+
+import entities.UserEntity;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.Type;
+
+@WebServlet(name = "adminCheck", value = "/adminCheck")
+public class AdminServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		UserEntity user = (UserEntity) session.getAttribute("user");
+		if(user.getUserType().equals(Type.Client)){
+			response.sendRedirect(request.getContextPath() + "/productList");
+		}else{
+			response.sendRedirect(request.getContextPath() + "/adminPage");
+		}
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
--- src/main/webapp/WEB-INF/admin.jsp
+++ src/main/webapp/WEB-INF/admin.jsp
@@ -1,9 +1,16 @@
+<%@ page import="entities.UserEntity" %>
+<%@ page import="model.Type" %>
 <%@ include file="taglibs.jspf"%>
 <html>
 <head>
     <title>Admin Page</title>
 </head>
 <body>
-
+<%
+    UserEntity user = (UserEntity) session.getAttribute("user");
+    if(user.getUserType().equals(Type.Client) || user == null){
+		response.sendRedirect(request.getContextPath() + "/productList");
+    }
+%>
 </body>
 </html>
